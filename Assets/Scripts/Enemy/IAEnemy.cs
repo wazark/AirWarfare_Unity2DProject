a372173f82fc665fd176c56b2f3a1e5eb0e9bc82
Diony Costa <68205351+wazark@users.noreply.github.com>
2022-12-13 18:57:00 +0000

    update AI Enemy

--- Assets/Scripts/Enemy/IAEnemy.cs
+++ Assets/Scripts/Enemy/IAEnemy.cs
@@ -7,61 +7,147 @@ public enum direction
 {
     Up, Down, Left, Right
 }
+public enum shootTimerSelect
+{
+    Fixed, Random
+}
+public enum reloadTimerSelect
+{
+    Fixed, Random
+}
+public enum ammoReloadSelect
+{
+    Fixed, Random
+}
+public enum startShoot
+{
+    immediately, wait
+}
 
 public class IAEnemy : MonoBehaviour
 {
+    [Header("Private Settings")]
+    private GameController _gameController;
+    private CharacterController _characterController;
+
     [Header("AI Movement Settings")]
     public float speedMove;
-    public float[] placeToCurve;
+    public float placeToCurve;
     public float curveDegress;
     public float increment;
     public direction movDirection;
-
-    [Header("AI Checks")]
-    public bool isMultiplesCurves; // working in progress;    
-    
-
+        
     [Header("Privates")]    
     private float incremented;    
     private float zRotation;    
     private bool isCurve;    
     private bool isArrived;
     private bool leftSide;
+    private bool isShooting;
+    
+    [Header("Weapon Transform")]
+    public Transform enemyWeapon;
+
+    [Header("Enemy Bullet Settings")]
+    public startShoot timerToStartShoot;
+    public float startShootCooldown;
+    public float bulletSpeed;
+    public float shootTimer;
+    public float reloadCooldown;
+    public int amountShoot;
+    public int maxShoot;
+
+    [Header("Type of Shoot Settings")]
+    
+    public ammoReloadSelect amountAmmoReloaded;
+    public int[] amountAmmoToReload;
+    public shootTimerSelect shotTimerType;
+    public float[] randShootTimer;
+    public reloadTimerSelect reloadTimerType;    
+    public float[] randReloadTimer;
 
 
     void Start()
     {
+        _gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        _characterController = FindObjectOfType(typeof(CharacterController)) as CharacterController;
+        
+
         zRotation = transform.eulerAngles.z;
     }
 
     
     void Update()
     {
-        movement();
+        singleMovementationCurve();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "playerShoot":
+
+                Destroy(collision.gameObject);
+                GameObject temp = Instantiate(_gameController.explosionPrefab, transform.position, transform.localRotation);
+                Destroy(temp, 0.5f);
+
+                spawnLoot();
+
+                Destroy(this.gameObject);
+
+                break;
+
+            case "Player":
+
+                temp = Instantiate(_gameController.explosionPrefab, transform.position, transform.localRotation);
+                Destroy(temp, 0.5f);
+
+                Destroy(this.gameObject);
+
+                break;
+        }
+    }
+
+    private void OnBecameVisible()
+    {
+        StartCoroutine("enemyStartShoot");
+    }
+    void spawnLoot()
+    {
+        int idItem;
+        int rand = Random.Range(0, 100);
+        if (rand < 50)
+        {
+            rand = Random.Range(0, 100);
+            if (rand > 85)
+            {
+                idItem = 2; // BombBox
+            }
+            else if (rand > 50)
+            {
+                idItem = 1; // HealthBox
+            }
+            else
+            {
+                idItem = 0; // CoinBox
+            }
+
+            Instantiate(_gameController.loot[idItem], transform.position, transform.localRotation);
+        }
     }
 
     void movement()
     {
-        
-        switch (isMultiplesCurves)
-        {
-            case false:
-                singleMovementationCurve();
-                break;
-
-                case true:
-
-                break;
-        }
-        
-
+         singleMovementationCurve();
     }
+
     void singleMovementationCurve()
     {
         switch(movDirection)
         {
             case direction.Up:
-                if (transform.position.y >= placeToCurve[0] && isCurve == false)
+                if (transform.position.y >= placeToCurve && isCurve == false)
                 {
                     isCurve = true;
                 }
@@ -69,7 +155,7 @@ public class IAEnemy : MonoBehaviour
                 break;
                                 
             case direction.Down:
-                if (transform.position.y <= placeToCurve[0] && isCurve == false)
+                if (transform.position.y <= placeToCurve && isCurve == false)
                 {
                     isCurve = true;
                 }
@@ -77,7 +163,7 @@ public class IAEnemy : MonoBehaviour
                 break;
 
             case direction.Left:
-                if (transform.position.x >= placeToCurve[0] && isCurve == false)
+                if (transform.position.x >= placeToCurve && isCurve == false)
                 {
                     isCurve = true;
                 }
@@ -85,7 +171,7 @@ public class IAEnemy : MonoBehaviour
                 break;
 
             case direction.Right:
-                if (transform.position.x <= placeToCurve[0] && isCurve == false)
+                if (transform.position.x <= placeToCurve && isCurve == false)
                 {
                     isCurve = true;
                 }
@@ -117,5 +203,91 @@ public class IAEnemy : MonoBehaviour
         }
     }
 
+    void Shoot()
+    {
+        enemyWeapon.right = _characterController.transform.position - transform.position;
+        if (isShooting == false && amountShoot > 0)
+        {
+            amountShoot--;
+            GameObject temp = Instantiate(_gameController.enemyBulletPrefab, enemyWeapon.position, enemyWeapon.localRotation);
+            temp.GetComponent<Rigidbody2D>().velocity = enemyWeapon.right * bulletSpeed;
+            temp.transform.up = _characterController.transform.position - transform.position;
+
+            if (amountShoot <= 0)
+            {
+                amountShoot = 0;
+                isShooting = true;
+                StartCoroutine("enemyReloadWeapon");
+            }
+
+            StartCoroutine("enemyShootTime");
+        }
+    }
+
+    IEnumerator enemyStartShoot()
+    {
+        switch(timerToStartShoot)
+        {
+            case startShoot.immediately:
+
+                yield return new WaitForSecondsRealtime(0.2f);
+                
+                break;
+
+            case startShoot.wait:
+
+                yield return new WaitForSecondsRealtime(startShootCooldown);
+                
+                break;
+        }
+        StartCoroutine("enemyShootTime");
+    }
+
+    IEnumerator enemyShootTime()  //delay entre cada tiro - random ou não
+    {
+        switch (shotTimerType)
+        {
+            case shootTimerSelect.Random:
+                yield return new WaitForSecondsRealtime(Random.Range(randShootTimer[0], randShootTimer[1]));
+                break;
+
+            case shootTimerSelect.Fixed:
+                yield return new WaitForSecondsRealtime(shootTimer);
+                break;
+        }
+
+        Shoot();
+    }
+    IEnumerator enemyReloadWeapon() //recarrega os tiros do inimigo conforme a bool de ser random ou não.
+    {
+        switch (reloadTimerType)
+        {
+            case reloadTimerSelect.Random:
+                yield return new WaitForSecondsRealtime(Random.Range(randReloadTimer[0], randReloadTimer[1]));
+                break;
+
+            case reloadTimerSelect.Fixed:
+                yield return new WaitForSecondsRealtime(reloadCooldown);
+                break;
+        }
+
+        switch (amountAmmoReloaded)
+        {
+            case ammoReloadSelect.Random:
+
+                amountShoot = Random.Range(amountAmmoToReload[0], amountAmmoToReload[1]);
+                isShooting = false;
+                Shoot();
+
+                break;
+
+            case ammoReloadSelect.Fixed:
+                amountShoot = maxShoot;
+                isShooting = false;
+                Shoot();
+                break;
+        }
+
+    }
     
 }
